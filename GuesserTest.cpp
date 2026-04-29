0aeb14b4daffa1d7c71285fbd795c5a6853b14a4
zigglethorp <cshansen@csuchico.edu>
2026-04-29 12:05:08 -0700

    Adding tests

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -14,9 +14,45 @@ class GuesserTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-// Example "smoke test" (can be deleted)
-TEST(GuesserTest, smoke_test)
-{
-  Guesser object("Secret");
-  ASSERT_EQ( 1+1, 2 );
+// // Example "smoke test" (can be deleted)
+// TEST(GuesserTest, smoke_test)
+// {
+//   Guesser object("Secret");
+//   ASSERT_EQ( 1+1, 2 );
+//}
+
+// Happy test Distance
+TEST(GuesserTest, DistanceHappy) {
+	unsigned int d;
+	EXPECT_EQ(d.distance(100), 10);
+}
+
+// Sad test Distance
+TEST(GuesserTest, DistanceSad) {
+	unsigned int d;
+	EXPECT_EQ(d.distance("-100"), -10);
+}
+
+// Invalid test Distance
+TEST(GuesserTest, DistanceInvalid) {
+	unsigned int d;
+	EXPECT_EQ(d.distance("Seven"), 0);
+}
+
+// Happy test Match
+TEST(GuesserTest, MatchHappy) {
+	match object m;
+	EXPECT_FALSE(m.match("password"));
+}
+
+// Invalid test Match
+TEST(GuesserTest, MatchSad) {
+	match object m;
+	EXPECT_FALSE(m.match("-12"));
+}
+
+// Invalid test Match
+TEST(GuesserTest, MatchInvalid) {
+	match object m;
+	EXPECT_FALSE(m.match("The password is far too difficult to find!"));
 }
